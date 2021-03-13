681fe834c2882bc175ee2807fe0c60197e27451c
rafdul <rafalduluk@gmail.com>
2021-03-13 16:46:09 +0100

    Add post testimonials

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+var uniqid = require('uniqid');
 
 const app = express();
 const db = [
@@ -6,19 +8,50 @@ const db = [
   { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
   { id: 3, author: 'Joseph Conrad', text: 'I think the knowledge came to him at last — only at the very last.'},
 ];
+const confirmation = { message: 'OK'};
 
-app.use(express.json());
 app.use(express.urlencoded( {extended: false} ));
-// app.use(cors());
+app.use(express.json());
+app.use(cors());
 
 app.get('/testimonials', (req, res) => {
   res.json(db);
+});
+
+app.get('/testimonials/random', (req, res) => {
+  const getRandomNumber = (max) => {
+    return Math.floor(Math.random() * (max + 1));
+  };
+  res.json(db[getRandomNumber(db.length)]);
 });
 
 app.get('/testimonials/:id', (req, res) => {
   const index = req.params.id;
   const indexNew = index.slice(1);
   res.json(db[indexNew - 1]);
+  // res.json(db.filter(item => item.id === req.params.id));
+});
+
+app.post('/testimonials', (req, res) => {
+  db.push({
+    id: uniqid(),
+    author: req.body.author,
+    text: req.body.text,
+  });
+  // res.json(req.body);
+  res.json(confirmation);
+});
+
+app.put('testimonilals/:id', (req, res) => {
+  const updatedItem = ({
+    id: req.params.id,
+    author: req.body.author,
+    text: req.body.text,
+  });
+  const itemToUptade = db.find(item => item.id === req.params.id);
+  const index = db.indexOf(itemToUptade);
+  db[index] = updatedItem;
+  res.json(confirmation);
 });
 
 app.listen(8080, () => {
