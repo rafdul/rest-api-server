1645d57f7bf28ebfa088f6d3cc2ca9e5cc97af0a
rafdul <rafalduluk@gmail.com>
2021-03-14 17:28:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const routerTestimonial = require('./routes/testimonials.routes');
 const routerConcerts = require('./routes/concerts.routes');
 const routerSeats = require('./routes/seats.routes');
@@ -9,15 +10,20 @@ const app = express();
 app.use(express.urlencoded( {extended: false} ));
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.use('/api/', routerTestimonial);
 app.use('/api/', routerConcerts);
 app.use('/api/', routerSeats);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
